f01e7c1b09e448a976623404e636ff93446745a3
Mikhail <jackglowpetux@gmail.com>
2020-03-20 23:41:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+﻿var hiragana = {
+	"i": "い",
+	"u": "う",
+	"e": "え",
+	"o": "お",
+	"ka": "か",
+	"ki": "き",
+	"ku": "く",
+	"ke": "け",
+	"ko": "こ",
+	"sa": "さ",
+	"shi": "し",
+	"su": "す",
+	"se": "せ",
+	"so": "そ",
+	"ta": "た",
+	"chi": "ち",
+	"tsu": "つ",
+	"te": "て",
+	"to": "と",
+	"na": "な",
+	"ni": "に",
+	"nu": "ぬ",
+	"ne": "ね",
+	"ho": "ほ",
+	"ma": "ま",
+	"mi": "み",
+	"mu": "む",
+	"me": "め",
+	"mo": "も",
+	"ya": "や",
+	"yu": "ゆ",
+	"yo": "よ",
+	"ra": "ら",
+	"ri": "り",
+	"ru": "る",
+	"re": "れ",
+	"ro": "ろ",
+	"wa": "わ",
+	"wo": "を",
+	"n": "ん",
+};
+var currentsym = null;
+var hiddentrans = false;
+
+var getRandomProperty = function (obj) {
+    var keys = Object.keys(obj);
+    return keys[ keys.length * Math.random() << 0];
+};
+function setCurrentSymbol(f)
+{
+	currentsym = f;
+}
+function setVisibility(boo)
+{
+	hiddentrans = boo;
+	if(boo)
+	{
+		document.querySelector('.symbol_trans').innerText = "      ";
+	}
+	else
+	{
+		document.querySelector('.symbol_trans').innerText = currentsym;
+	}
+}
+function displayRandomHiragana()
+{
+	setCurrentSymbol(getRandomProperty(hiragana));
+	document.querySelector('.symbol').innerText = hiragana[currentsym];
+	if(hiddentrans)
+	{
+		document.querySelector('.symbol_trans').innerText = "      ";
+	}
+	else
+	{
+		document.querySelector('.symbol_trans').innerText = currentsym;
+	}
+}
+
+window.onload = function()
+{
+	displayRandomHiragana();
+}
